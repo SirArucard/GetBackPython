637df6bd4407d2e880e0660e8aa15b4a07214508
Vinícius Prata <108702285+SirArucard@users.noreply.github.com>
2025-11-18 13:50:22 +0000

    Mete bala pelo amor de deus

--- ex04_lists.py
+++ ex04_lists.py
@@ -2,6 +2,7 @@ listinha = []
 for i in range (1, 6):
     numero = int(input("Me dê os números: "))
     listinha.append(numero)
+print("Teste")
 
 print(f"Maior numero: {max(listinha)}")
 print(f"Menor numero: {min(listinha)}")

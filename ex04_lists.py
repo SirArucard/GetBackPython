fa84e8bbd3c946250d345210cea80d0afe39cfa9
Vinícius Prata <108702285+SirArucard@users.noreply.github.com>
2025-11-18 13:48:07 +0000

    Exercicios finalizados

---
+++ ex04_lists.py
@@ -0,0 +1,9 @@
+listinha = []
+for i in range (1, 6):
+    numero = int(input("Me dê os números: "))
+    listinha.append(numero)
+
+print(f"Maior numero: {max(listinha)}")
+print(f"Menor numero: {min(listinha)}")
+print(f"A soma é: {sum(listinha)}")
+print(f"Organizadin: {sorted(listinha)}")
